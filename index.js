ffd40847e1a8a5f64570302cf7ea4b1d20e9226f
ehalterman <hal17034@byui.edu>
2020-11-14 22:14:16 -0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const port = 5000
 
 app.use(express.static(__dirname + '/public'));
 
@@ -87,11 +87,12 @@ function calculateRate(res, weight, type) {
           result = 5.90;
         }
       }
-    }
+    
     const params = {
-      weight: weight,
       method: type,
+      weight: weight,
       result: result
     };
 
-    response.render('pages/result', params);
+    res.render('pages/result', params);
+  }
